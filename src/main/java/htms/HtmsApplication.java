2cd400e9d6ac22e1448de75e0658991513dda93a
VienQuocBinh <91947000+VienQuocBinh@users.noreply.github.com>
2023-06-02 22:56:33 +0700

    init

---
+++ src/main/java/htms/HtmsApplication.java
@@ -0,0 +1,13 @@
+package htms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HtmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HtmsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/htms/HtmsApplicationTests.java
@@ -0,0 +1,13 @@
+package htms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HtmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
