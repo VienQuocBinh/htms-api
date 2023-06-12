1790e6b2e31d1246aacd3b1ca57d5acb85b2c8c7
noelvu <thuyenvuanh2412@gmail.com>
2023-06-12 20:23:22 +0700

    add docker services to project

--- src/main/resources/db/migration/V1__create_table.sql
+++ src/main/resources/db/migration/V1__create_table.sql
@@ -1,30 +1,3 @@
-create sequence public.option_seq
-    increment by 50;
-
-alter sequence public.option_seq owner to "user";
-
-create table public.flyway_schema_history
-(
-    installed_rank integer                 not null
-        constraint flyway_schema_history_pk
-            primary key,
-    version        varchar(50),
-    description    varchar(200)            not null,
-    type           varchar(20)             not null,
-    script         varchar(1000)           not null,
-    checksum       integer,
-    installed_by   varchar(100)            not null,
-    installed_on   timestamp default now() not null,
-    execution_time integer                 not null,
-    success        boolean                 not null
-);
-
-alter table public.flyway_schema_history
-    owner to "user";
-
-create index flyway_schema_history_s_idx
-    on public.flyway_schema_history (success);
-
 create table public.changed_schedule
 (
     created_date       timestamp(6) not null,
