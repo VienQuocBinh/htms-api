2e10c97781a975723653b5ce81e7add89abfdff7
noelvu <thuyenvuanh2412@gmail.com>
2023-06-22 22:59:28 +0700

    resolve conflicts

--- src/main/java/htms/service/impl/ProgramServiceImpl.java
+++ src/main/java/htms/service/impl/ProgramServiceImpl.java
@@ -4,10 +4,7 @@ import htms.api.request.ProgramRequest;
 import htms.api.response.ProgramResponse;
 import htms.api.response.TopicDetail;
 import htms.api.response.TrainerResponse;
-import htms.common.mapper.ProgramMapper;
 import htms.model.Program;
-import htms.model.ProgramContent;
-import htms.model.embeddedkey.ProgramContentId;
 import htms.repository.ClassRepository;
 import htms.repository.ProgramContentRepository;
 import htms.repository.ProgramRepository;
@@ -15,10 +12,8 @@ import htms.service.ProgramService;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
-import org.springframework.data.crossstore.ChangeSetPersister;
 import org.springframework.stereotype.Service;
 
-import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import java.util.UUID;
@@ -27,7 +22,6 @@ import java.util.UUID;
 @RequiredArgsConstructor
 public class ProgramServiceImpl implements ProgramService {
     private final ProgramRepository programRepository;
-    private final ProgramMapper programMapper;
     private final ClassRepository classRepository;
     private final ProgramContentRepository contentRepository;
     private final ModelMapper mapper;
@@ -64,7 +58,7 @@ public class ProgramServiceImpl implements ProgramService {
     public List<ProgramResponse> getProgramsTheTraineeIsTaking(UUID traineeId) {
         var classes = classRepository.findAllCurrentTakingClassesByTrainee(traineeId).orElse(Collections.emptyList());
         return classes.stream().map(c -> {
-            var model = programMapper.toResponse(c.getProgram());
+            var model = mapper.map(c.getProgram(), ProgramResponse.class);
             model.setTrainer(mapper.map(c.getTrainer(), TrainerResponse.class));
             return model;
         }).toList();
@@ -73,7 +67,7 @@ public class ProgramServiceImpl implements ProgramService {
     @Override
     public ProgramResponse getProgramContent(UUID programId, UUID trainerId) {
         var content = contentRepository.findById_Program_IdAndId_Trainer_Id(programId, trainerId).stream().findFirst().orElseThrow(EntityNotFoundException::new);
-        var response = programMapper.toResponse(content.getId().getProgram());
+        var response = mapper.map(content.getId().getProgram(), ProgramResponse.class);
         response.setTopics(content.getTopics().stream().map(topic -> mapper.map(topic, TopicDetail.class)).toList());
         response.setTrainer(mapper.map(content.getId().getTrainer(), TrainerResponse.class));
         return response;
