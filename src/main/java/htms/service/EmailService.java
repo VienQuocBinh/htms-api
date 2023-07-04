fb3565103f6fd80318835331f306b851b0e59305
VienQuocBinh <91947000+VienQuocBinh@users.noreply.github.com>
2023-07-05 02:33:54 +0700

    Additional commit for the previous.

--- src/main/java/htms/service/EmailService.java
+++ src/main/java/htms/service/EmailService.java
@@ -4,6 +4,4 @@ import htms.api.domain.EmailDetails;
 
 public interface EmailService {
     void sendSimpleEmail(EmailDetails details);
-
-    String sendMailWithAttachment(EmailDetails details);
 }
